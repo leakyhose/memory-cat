6ca2dc59fc23bd4fda9a9f473405f0b596eb50cc
leakyhose <yimingsu2007@gmail.com>
2025-11-06 16:06:28 -0800

    Add main components and order in App.jsx - Add ability to change between game state and endgame state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Scoreboard'
+import Scoreboard from './components/Scoreboard'
+import Gameboard from './components/Gameboard'
+import EndScreen from './components/EndScreen'
 
 function App() {
-  const [gifs, setGifs] = useState([]);
-  const [level, setLevel] = useState([]);
+  const [level, setLevel] = useState(0);
+  const [highLevel, setHighLevel] = useState(0);
+  const [screenState, setScreenState] = useState("game")
 
-  return 
+  function restart (){
+    if (level > highLevel){
+      setHighLevel(level);
+      setLevel = 0;
+    }
+  }
+
+  return(
+  <div className = "main">
+    {
+      screenState == "game" ? 
+      (
+        <>
+          <Header/>
+          <Scoreboard/>
+          <Gameboard/>
+        </>
+      ):
+      (
+        <>
+          <Header/>
+          <EndScreen/>
+        </>
+      )
+    }
+  </div>)
 }
 
 export default App
