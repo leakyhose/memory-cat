7b57eaeb8e41f33add5d6617da28c85ef2a95d59
leakyhose <yimingsu2007@gmail.com>
2025-11-06 17:56:52 -0800

    Modify card model to include whether card has been seen

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -9,22 +9,35 @@ export default function GameBoard (
         setScreenState
     }
 ){ 
-    const [card, setCard] = useState({});
+    const [card, setCard] = useState({
+        url: "",
+        seen: false
+    });
     const [seenCards, setSeenCards] = useState([]);
 
     async function chooseCard() {
         if (seenCards.length > 4 && checkChance(40)){
-            setCard(getRandomArray(seenCards));
+            setCard({
+                url: getRandomArray(seenCards),
+                seen: true
+            });
         }
         else{
-            setCard(getCat());
-            setSeenCards(prev => [...prev, card]);
+            setCard({
+                url: getCat(),
+                seen: false
+            });
+            setSeenCards(prev => [...prev, card.url]);
         }
     }
+
+    
     
     useEffect(() => {
         chooseCard();
     }, []);
+
+
 
     return <div>
         <img src={card} />
